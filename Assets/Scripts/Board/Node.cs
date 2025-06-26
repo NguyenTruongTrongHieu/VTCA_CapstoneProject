4fbbaddda52e394073ce4c7e47157002647549d6
thien2408387 <quangthientran3142@gmail.com>
2025-06-26 10:50:39 +0700

    Create Food Board
    - Create food board with background

--- Assets/Scripts/Board/Node.cs
+++ Assets/Scripts/Board/Node.cs
@@ -6,19 +6,20 @@ public class Node : MonoBehaviour
 {
    public bool isOccupied; // biến kiểm tra xem ô có bị chiếm hay không
 
-    //public GameObject cell; // biến chứa prefab của ô
+    public GameObject food; // biến chứa prefab của ô
 
-    public int xIndex; // biến chứa chỉ số hàng của ô
-    public int yIndex; // biến chứa chỉ số cột của ô
+    //public int xIndex; // biến chứa chỉ số hàng của ô
+    //public int yIndex; // biến chứa chỉ số cột của ô
 
-    public Node(bool _isOccupied)
+    public Node (bool _isOcupied, GameObject _food)
     {
-        isOccupied = _isOccupied;
+        isOccupied = _isOcupied;
+        food = _food;   
     }
 
-    public void SetIndex(int _xIndex, int _yIndex)
-    {
-        xIndex = _xIndex;
-        yIndex = _yIndex;
-    }
+    //public void SetIndex(int _xIndex, int _yIndex)
+    //{
+    //    xIndex = _xIndex;
+    //    yIndex = _yIndex;
+    //}
 }
