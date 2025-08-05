2d712c0cd9cde1f9ee934b828660219326b0b62c
NguyenTruongTrongHieu <tronghieutruong5102004@gmail.com>
2025-08-05 10:30:17 +0700

    Fix player movement

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -44,14 +45,14 @@ public class Player : MonoBehaviour
                                 enemyType != EnemyType.boss ? stopDistanceWithEnemy : stopDistanceWithBoss)),
 
 
-                            new RotateToTarget(this.transform,
-                            LevelManager.instance.currentLevel.enemiesAtLevel[GameManager.instance.currentEnemyIndex].transform, 3f),
+                            //new RotateToTarget(this.transform,
+                            //LevelManager.instance.currentLevel.enemiesAtLevel[GameManager.instance.currentEnemyIndex].transform, 3f),
 
                             new PlayerMoveToTarget(this,
                                 LevelManager.instance.currentLevel.enemiesAtLevel[GameManager.instance.currentEnemyIndex].transform,
                                 (LevelManager.instance.currentLevel.
                                 enemiesAtLevel[GameManager.instance.currentEnemyIndex].GetComponent<EnemyStat>().
-                                enemyType != EnemyType.boss ? stopDistanceWithEnemy : stopDistanceWithBoss))
+                                enemyType != EnemyType.boss ? stopDistanceWithEnemy : stopDistanceWithBoss), 300f)
                         }),
 
                         //new Sequence(new List<NodeBehaviourTree>//Attack
@@ -113,16 +114,40 @@ public class PlayerMoveToTarget : NodeBehaviourTree
     private Player playerSelf;
     private Transform target;
     private float stopDistance;
+    private float rotationSpeed; // Speed of rotation towards the target
 
-    public PlayerMoveToTarget(Player player, Transform target, float stopDistance)
+    public PlayerMoveToTarget(Player player, Transform target, float stopDistance, float rotationSpeed)
     {
         this.target = target;
         this.playerSelf = player;
         this.stopDistance = stopDistance;
+        this.rotationSpeed = rotationSpeed;
     }
 
     public override NodeState Evaluate()
     {
+        playerSelf.animator.SetBool(playerSelf.isMovingHash, true);
+
+        //Rotate towards target
+        //if ()
+        //{
+        //    Vector3 direction = target.position - playerSelf.transform.position;
+        //    Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+        //    Player player = playerSelf.transform.GetComponent<Player>();
+
+        //    Vector3 rotation = Quaternion.Lerp(playerSelf.transform.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
+        //    playerSelf.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        //}
+
+        Vector3 direction = target.position - playerSelf.transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        float dot = Vector3.Dot(playerSelf.transform.forward, direction.normalized);
+        if (dot <= 0.99f)
+        {
+            playerSelf.transform.rotation = Quaternion.RotateTowards(playerSelf.transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+        }
+
         //Move player towards target
         //Vector3 direction = (target.position - playerSelf.transform.position).normalized;
         if (Vector3.Distance(playerSelf.transform.position, target.position) > stopDistance)
