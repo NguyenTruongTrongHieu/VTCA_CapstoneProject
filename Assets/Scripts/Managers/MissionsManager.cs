1ea28c965ec07a7af395255a573ef69b4b2f786d
thien2408387 <quangthientran3142@gmail.com>
2025-08-05 09:16:03 +0700

    Add Current Level Display + Add Skin for Player 1
    - Now can display current level in game
    - has 1 skin for player 1

---
+++ Assets/Scripts/Managers/MissionsManager.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionsManager : MonoBehaviour
+{
+    public static MissionsManager _instance;
+
+    public Text[] missionsDescription;
+
+
+
+    public void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMissionDescription(int missionIndex, string description)
+    {
+        if (missionIndex < 0 || missionIndex >= missionsDescription.Length)
+        {
+            Debug.LogError("Mission index out of bounds");
+            return;
+        }
+        missionsDescription[missionIndex].text = description;
+    }
+}
