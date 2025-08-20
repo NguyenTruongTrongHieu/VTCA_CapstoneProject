2977a05b091ea24de9ea6285aa14e224a4f94bf1
NguyenTruongTrongHieu <tronghieutruong5102004@gmail.com>
2025-08-20 13:38:59 +0700

    Add chest box manager script
    Fix: fix hp and mana slider can be interacted with

---
+++ Assets/Scripts/Chest/ChestManager.cs
@@ -0,0 +1,137 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChestType
+{
+    Common,
+    Rare,
+    Epic,
+    Legendary
+}
+
+
+public class ChestManager : MonoBehaviour
+{
+    public ChestType chestType;
+    [Header("Rewards in chest box after open")]
+    public int amountOfItemRewards;
+    public List<string> itemRewardsInChestBox = new List<string>();//used as item class
+    public int crystals;
+    public int coins;
+    public int stars;
+
+    [Header("All rewards in chest box")]
+    [Header("Currency rewards")]
+    public int minCrystal, maxCrystal;
+    public int minCoin, maxCoin;
+    public int minStar, maxStar;
+
+    [Header("Item rewards")]
+    public ItemLevel[] itemLevelsInChest;
+    public int[] dropItemRate;
+    public ItemType[] itemTypesInChest;
+    public GameObject itemUIPrefab;
+
+    private void Start()
+    {
+        //Initialize the chest box with rewards
+        SetRewardForChestBox();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O)) // For testing purposes, press 'O' to open the chest
+        {
+            OpenChest();
+        }
+    }
+
+    public void OpenChest()
+    {
+        if (crystals > 0)
+        {
+            CurrencyManager.instance.AddCrystalsDontHaveAnim(crystals);
+        }
+        if (coins > 0)
+        {
+            CurrencyManager.instance.AddCoinsDontHaveAnim(coins);
+        }
+        if (stars > 0)
+        {
+            CurrencyManager.instance.AddStarsDontHaveAnim(stars);
+        }
+
+        foreach (var itemReward in itemRewardsInChestBox)
+        {
+            Debug.Log($"Reward Item: {itemReward}");
+        }
+
+        SetRewardForChestBox();
+    }
+    public void SetRewardForChestBox()
+    { 
+        //Set currency rewards
+        if(minCrystal == maxCrystal)
+        {
+            if (minCrystal != 0)
+            {
+                crystals = minCrystal;
+            }
+        }
+        else
+        {
+            crystals = Random.Range(minCrystal, maxCrystal + 1);
+        }
+        if (minCoin == maxCoin)
+        {
+            if (minCoin != 0)
+            {
+                coins = minCoin;
+            }
+        }
+        else
+        {
+            coins = Random.Range(minCoin, maxCoin + 1);
+        }
+        if (minStar == maxStar)
+        {
+            if (minStar != 0)
+            {
+                stars = minStar;
+            }
+        }
+        else
+        {
+            stars = Random.Range(minStar, maxStar + 1);
+        }
+
+
+        if (amountOfItemRewards > 0)
+        { 
+            itemRewardsInChestBox.Clear(); // Clear previous rewards if any
+            for(int i = 0; i < amountOfItemRewards; i++)
+            {
+                RandomItemForChestBox();
+            }
+        }
+    }
+
+    public void RandomItemForChestBox()
+    {
+        int rate = Random.Range(0, 101);
+        for (int i = 0; i < itemLevelsInChest.Length; i++)
+        { 
+            if(rate <= dropItemRate[i])
+            {
+                ItemLevel itemLevel = itemLevelsInChest[i];
+                ItemType itemType = itemTypesInChest[i];
+
+                //Add itemLEvel and ItemType to itemRewardsInChestBox
+                string itemClass = $"{itemLevel}_{itemType}";
+                itemRewardsInChestBox.Add(itemClass);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Chest/UIRewardInChestBox.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRewardInChestBox : MonoBehaviour
+{
+    [Header("Item info UI")]
+    public Image itemIcon;
+    public Image levelItemImage;
+
+    [Header("Item level sprites")]
+    public Sprite whiteLevelSprite;
+    public Sprite greenLevelSprite;
+    public Sprite blueLevelSprite;
+    public Sprite purpleLevelSprite;
+    public Sprite orangeLevelSprite;
+
+    [Header("ItemIcon")]
+    public Sprite sword;
+    public Sprite helmet;
+    public Sprite armor;
+    public Sprite boots;
+
+
+}
