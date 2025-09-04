ec347c0fc2c261c1eed533beb26cc2507a358835
NguyenTruongTrongHieu <tronghieutruong5102004@gmail.com>
2025-09-04 23:22:27 +0700

    Update Player.cs

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -22,6 +22,7 @@ public class Player : MonoBehaviour
     private NodeBehaviourTree rootNode;
 
     [Header("Boss SFX")]
+    public string[] bossVoice;
 
     public void SetUpBehaviourTree()
     {
